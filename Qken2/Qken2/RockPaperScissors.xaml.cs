da523e3f647ef79a9ab68b2449af7edad528c5ed
Andrej Svrček <nepoviem251@gmail.com>
2022-04-07 21:48:30 +0200

    Completed Rock Paper Scissors.

--- Qken2/Qken2/RockPaperScissors.xaml.cs
+++ Qken2/Qken2/RockPaperScissors.xaml.cs
@@ -12,34 +12,118 @@ namespace Qken2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RockPaperScissors : ContentPage
     {
+        int wins = 0;
         public RockPaperScissors()
         {
             InitializeComponent();
         }
-
-        private void Button_Clickedback(object sender, EventArgs e)
+       
+       private void Button_Clickedback(object sender, EventArgs e)
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
         }
-
-        private void Button_Clicked_virzdek(object sender, EventArgs e)
-        {
-
-        }
-
         private void Button_Clicked_rock(object sender, EventArgs e)
         {
+            Random random = new Random();
+            int opponentmove = random.Next(1, 4);
 
+            if (opponentmove == 1)
+            {
+                Opponent.Text = "Rock";
+            }
+            else if (opponentmove == 2)
+            {
+                Opponent.Text = "Paper";
+            }
+            else
+            {
+                Opponent.Text = "Scissors";
+            }
+
+            if (opponentmove == 1)
+            {
+                Winlose.Text = "You Draw";
+            }
+            else if (opponentmove == 2)
+            {
+                Winlose.Text = "You Lose";
+            }
+            else
+            {
+                 Winlose.Text = "You Win";
+                wins += 1;
+                Wins.Text = wins.ToString();
+            }
+            
         }
 
         private void Button_Clicked_paper(object sender, EventArgs e)
         {
+            Random random = new Random();
+            int opponentmove = random.Next(1, 4);
 
+            if (opponentmove == 1)
+            {
+                Opponent.Text = "Rock";
+            }
+            else if (opponentmove == 2)
+            {
+                Opponent.Text = "Paper";
+            }
+            else
+            {
+                Opponent.Text = "Scissors";
+            }
+
+            if (opponentmove == 1)
+            {
+                Winlose.Text = "You Win";
+                wins += 1;
+                Wins.Text = wins.ToString();
+            }
+            else if (opponentmove == 2)
+            {
+                Winlose.Text = "You Draw";
+            }
+            else
+            {
+                Winlose.Text = "You Lose";
+            }
         }
 
         private void Button_Clicked_scissors(object sender, EventArgs e)
         {
+            Random random = new Random();
+            int opponentmove = random.Next(1, 4);
 
+            if (opponentmove == 1)
+            {
+                Opponent.Text = "Rock";
+            }
+            else if (opponentmove == 2)
+            {
+                Opponent.Text = "Paper";
+            }
+            else
+            {
+                Opponent.Text = "Scissors";
+            }
+
+            if (opponentmove == 1)
+            {
+                Winlose.Text = "You Lose";
+            }
+            else if (opponentmove == 2)
+            {
+                Winlose.Text = "You Win";
+                wins += 1;
+                Wins.Text = wins.ToString();
+            }
+            else
+            {
+                Winlose.Text = "You Draw";
+            }
         }
+       
     }
 }
