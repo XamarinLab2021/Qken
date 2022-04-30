f3e58aaa0e1688a7a252d9603a51b5e153fee3cc
Andrej Svrček <nepoviem251@gmail.com>
2022-04-30 16:36:08 +0200

    Troska prerobene piskvorky

--- Qken2/Qken2/DudoClicker.xaml.cs
+++ Qken2/Qken2/DudoClicker.xaml.cs
@@ -22,7 +22,7 @@ namespace Qken2
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void Button_ClickedBack(object sender, EventArgs e)
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
         }
--- Qken2/Qken2/TicTacToe.xaml.cs
+++ Qken2/Qken2/TicTacToe.xaml.cs
@@ -13,10 +13,123 @@ namespace Qken2
     {
         bool multiplayer = false;
         int xoro = 1;
+        bool bzero = false;
+        bool bone = false;
+        bool btwo = false;
+        bool bthree = false;
+        bool bfour = false;
+        bool bfive = false;
+        bool bsix = false;
+        bool bseven = false;
+        bool beight = false;
+
+        
         
         public TicTacToe()
         {
             InitializeComponent();
+        }
+
+        public void ImageDisplay()
+        {
+            xoro += 1;
+
+            if (xoro % 2 == 0)
+            {
+                if (bzero == true)
+                {
+                    zero.ImageSource = "X.jpg";
+                    bzero = false;
+                }
+                else if (bone == true)
+                {
+                    one.ImageSource = "X.jpg";
+                    bone = false;
+                }
+                else if (btwo == true)
+                {
+                    two.ImageSource = "X.jpg";
+                    btwo = false;
+                }
+                else if (bthree == true)
+                {
+                    three.ImageSource = "X.jpg";
+                    bthree = false;
+                }
+                else if (bfour == true)
+                {
+                    four.ImageSource = "X.jpg";
+                    bfour = false;
+                }
+                else if (bfive == true)
+                {
+                    five.ImageSource = "X.jpg";
+                    bfive = false;
+                }
+                else if (bsix == true)
+                {
+                    six.ImageSource = "X.jpg";
+                    bsix = false;
+                }
+                else if (bseven == true)
+                {
+                    seven.ImageSource = "X.jpg";
+                    bseven = false;
+                }
+                else if (beight == true)
+                {
+                    eight.ImageSource = "X.jpg";
+                    beight = false;
+                }
+            }
+            else
+            {
+                if (bzero == true)
+                {
+                    zero.ImageSource = "O.jpg";
+                    bzero = false;
+                }
+                else if (bone == true)
+                {
+                    one.ImageSource = "O.jpg";
+                    bone = false;
+                }
+                else if (btwo == true)
+                {
+                    two.ImageSource = "O.jpg";
+                    btwo = false;
+                }
+                else if (bthree == true)
+                {
+                    three.ImageSource = "O.jpg";
+                    bthree = false;
+                }
+                else if (bfour == true)
+                {
+                    four.ImageSource = "O.jpg";
+                    bfour = false;
+                }
+                else if (bfive == true)
+                {
+                    five.ImageSource = "O.jpg";
+                    bfive = false;
+                }
+                else if (bsix == true)
+                {
+                    six.ImageSource = "O.jpg";
+                    bsix = false;
+                }
+                else if (bseven == true)
+                {
+                    seven.ImageSource = "O.jpg";
+                    bseven = false;
+                }
+                else if (beight == true)
+                {
+                    eight.ImageSource = "O.jpg";
+                    beight = false;
+                }
+            }
         }
 
         private void Button_ClickedBack(object sender, EventArgs e)
@@ -36,119 +149,65 @@ namespace Qken2
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                nula.ImageSource = "X.jpg";
-            }
-            else
-            {
-                nula.ImageSource = "O.jpg";
-            }
+            
+            bzero = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                jedna.ImageSource = "X.jpg";
-            }
-            else
-            {
-                jedna.ImageSource = "O.jpg";
-            }
+            
+            bone = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                dva.ImageSource = "X.jpg";
-            }
-            else
-            {
-                dva.ImageSource = "O.jpg";
-            }
+            
+            btwo = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_3(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                tri.ImageSource = "X.jpg";
-            }
-            else
-            {
-                tri.ImageSource = "O.jpg";
-            }
+            
+            bthree = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_4(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                styri.ImageSource = "X.jpg";
-            }
-            else
-            {
-                styri.ImageSource = "O.jpg";
-            }
+            
+            bfour = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_5(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                pat.ImageSource = "X.jpg";
-            }
-            else
-            {
-                pat.ImageSource = "O.jpg";
-            }
+            
+            bfive = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_6(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                sest.ImageSource = "X.jpg";
-            }
-            else
-            {
-                sest.ImageSource = "O.jpg";
-            }
+            
+            bsix = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_7(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                sedem.ImageSource = "X.jpg";
-            }
-            else
-            {
-                sedem.ImageSource = "O.jpg";
-            }
+            
+            bseven = true;
+            ImageDisplay();
         }
 
         private void Button_Clicked_8(object sender, EventArgs e)
         {
-            xoro += 1;
-            if (xoro % 2 == 0)
-            {
-                osem.ImageSource = "X.jpg";
-            }
-            else
-            {
-                osem.ImageSource = "O.jpg";
-            }
+            
+            beight = true;
+            ImageDisplay();
         }
     }
 }
