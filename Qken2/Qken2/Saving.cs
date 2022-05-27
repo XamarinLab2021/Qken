418a09f51f703eb8b8940a899bf9a378ee2ee94e
exectionik <jakkappro@gmail.com>
2022-05-27 09:12:34 +0200

    Buttons on MainPage

---
+++ Qken2/Qken2/Saving.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Qken2
+{
+    class Saving
+    {
+        public int dudpoints;
+    }
+}
